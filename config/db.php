9a83dcb5f43734c5721ea1a91979cd16c84a076c
Ghiya Mikadze <g.mikadze@lakka.io>
2024-11-20 13:36:10 +0300

    начальный коммит

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=weirdbirru_jobtest',
+    'username' => 'weridbirru',
+    'password' => 'BxD2FDZBCQF_8N5S',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
